cfb2cfbe0ed7ebf114e571357fe7ccb8e3fa1b64
fgsdfg <69824747+fgsdfgesfs@users.noreply.github.com>
2025-02-17 13:30:49 +0500

    Update browser.py

--- browser.py
+++ browser.py
@@ -2,15 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-# Set up Bromite as the browser
 chrome_options = Options()
-chrome_options.binary_location = "/data/data/com.termux/files/usr/bin/bromite"
+chrome_options.binary_location = "/data/data/com.termux/files/usr/bin/bromite"  # Bromite browser
 chrome_options.add_argument("--headless")  # Run in headless mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Start WebDriver
-service = Service("/data/data/com.termux/files/usr/bin/geckodriver")  # Geckodriver is needed for some functionality
+service = Service("/data/data/com.termux/files/usr/bin/chromedriver")  # Path to chromedriver
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Open a website
